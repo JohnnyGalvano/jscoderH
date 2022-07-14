24aec1ed43e61cc37ac7a99daf2314408e48d462
Jonatan Galvano <jonidani12@gmail.com>
2022-07-13 23:30:05 -0300

    simulador interactivo. Primer desafio entregable

--- js/index.js
+++ js/index.js
@@ -1,11 +1,68 @@
-//este programa hace una pregunta al usuario el cual debe responder correctamente para ingresar al sitio//
-let usuario = prompt (`Bienvenido! Ingrese su nombre por favor`)
-let trivia = prompt (`Bienvenido ${usuario} para continuar debes responder correctamente la siguiente pregunta:
-                      ¿Cual es la capital de Italia?`)
-while (trivia != `Roma`){
-  trivia = prompt (`Respuesta incorrecta. Intentemos de nuevo:
-                      ¿Cual es la capital de Italia?`)
+//Cotizador de presupuestos de reparacion de electrodomesticos//
+inicio ()
+function inicio (){
+let equipo = prompt (`Bienvenido! Ingrese el equipo para comenzar:
+                            1. HELADERA
+                            2. LAVARROPAS`)
+if(equipo == Number (1)){
+    heladera()
+} else if(equipo == Number (2)) {
+    lavarropas ()
+}else {
+    alert (`Ingrese una opcion valida`)
+    inicio ()
 }
-if (trivia == `Roma`){
-  alert (`Felicitaciones ${usuario}! Respuesta correcta!`)
 }
+function heladera (){
+        const tecnologia = prompt (`seleccione la tecnologia de enfriamiento:
+                            NO FROST
+                            CICLICA`)
+        const marca = prompt (`seleccione la marca del equipo:
+                                WHIRLPOOL
+                                LG
+                                SAMSUNG`)
+        const falla = prompt (`Seleccione la averia:
+                                NO FEEZA/ENFRIA
+                                NO ARRANCA
+                                CONGELA EN HELADERA
+                                OTRA FALLA`)
+                                if (falla == `OTRA FALLA`){
+                                    alert (`Debe comunicarse con nuestro servicio de atencios al cliente`)
+                                } else if (falla == `NO FREEZA/ENFRIA`){
+                                    alert (`El costo de reparar su heladera ${tecnologia} ${marca} es de $45000`)
+                                } else if (falla == `NO ARRANCA`){
+                                    alert (`El costo de reparar su heladera ${tecnologia} ${marca} es de $20000`)
+                                } else {
+                                    alert (`El costo de reparar su heladera ${tecnologia} ${marca} es de $30000`)
+                                }
+    }
+function lavarropas (){
+    const tecnologia = prompt (`ingrese la tecnologia de lavado de su lavarropas:
+                                ELECTRONICO
+                                RELOJ
+                                INVERTER`)
+    const marca = prompt (`ingrese la marca de su lavarropas:
+                            DREAN
+                            WHIRLPOOL
+                            SAMSUNG
+                            LG`)
+    const falla = prompt (`ingrese la falla de su lavarropas:
+                            1. NO LAVA
+                            2. HACE MUCHO RUIDO
+                            3. NO ENCIENDE
+                            4. OTRA FALLA`)
+                            if (falla == Number (1)){
+                                alert (`El costo de la reparacion de su lavarropas ${tecnologia} ${marca} es de $28000`)
+                            } else if (falla == Number (2)){
+                                alert (`El costo de la reparacion de su lavarropas ${tecnologia} ${marca} es de $25000 a $30000`)
+                            } else if (falla == Number (3)){
+                                alert (`El costo de la reparacion de su lavarropas ${tecnologia} ${marca} es de $30000`)
+                            } else if (falla == Number (4)){
+                                alert (`Debe comunicarse con nuestro servicio de atencion al cliente`)
+                            } else {
+                                alert (`ingrese una opcion valida`)
+                                inicio()
+                            }
+
+}
+alert (`Los presupuestos aqui expresados estan sujetos a verificacion por parte de nuestro staff tecnico`)
